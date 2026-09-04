d5fbc43bc8b5d8d1d12b4aae9b3576d0cf1d8d83
mirmakhmud5051 <mirmahmudjunaydullayev@gmail.com>
2026-09-04 23:09:10 +0500

    header/footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div>
+    <div className=' container flex flex-col  items-center py-[25px]'>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
+        
       </Routes>
+      <Footer/>
     </div>
   );
 }
