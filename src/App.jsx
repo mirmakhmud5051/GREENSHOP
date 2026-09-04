b973db5603ed41fd7a2e72ed36907839ebcc231b
Osimxon <osimhondadaxanov2013@gmail.com>
2026-09-04 21:14:04 +0500

    addded file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Cart from "./pages/cart/Cart";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
