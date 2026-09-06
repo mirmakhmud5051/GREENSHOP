124fc7d52f43fd80eee84c4f5058992de1dba3e5
murodjon0987 <wenzonepeser@gmail.com>
2026-09-06 08:54:10 +0500

    JSX

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className=' container flex flex-col  items-center py-[25px]'>
       <Header/>
-      <Home/>
+      {/* <Home/> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
