cf4532239208e4f630331a2a052b7084370bfd18
mirmakhmud5051 <mirmahmudjunaydullayev@gmail.com>
2026-09-06 11:08:04 +0500

    checkout added

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/cart" element={<Cart />} />
-        
+      <Route path='/checkout' element={<Checkout/>}/>
       </Routes>
       <Footer/>
     </div>
