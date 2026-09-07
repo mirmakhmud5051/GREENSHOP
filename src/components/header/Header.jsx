3092f65f30bc6d5be0416359aa9e4d4e012e3e87
mirmakhmud5051 <mirmahmudjunaydullayev@gmail.com>
2026-09-07 20:49:43 +0500

    added cart

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,25 +12,36 @@ function Header() {
         <ul className=" flex items-center gap-[50px] ">
           <Link
             className=" font-bold text-[16px]  text-[rgba(61,61,61,1)] border-b border-b-2 border-[rgba(70,163,88,1)]"
-          to={"/"}
+            to={"/"}
           >
             Home
           </Link>
-          <Link className=" font-normal text-[rgba(61,61,61,1)] text-[16px] " to={"/shop"}>
+          <Link
+            className=" font-normal text-[rgba(61,61,61,1)] text-[16px] "
+            to={"/shop"}
+          >
             Shop
           </Link>
-          <Link className=" font-normal text-[rgba(61,61,61,1)] text-[16px] " >
+          <Link className=" font-normal text-[rgba(61,61,61,1)] text-[16px] ">
             Plant Care
           </Link>
           <Link className=" font-normal text-[rgba(61,61,61,1)] text-[16px] ">
             Blogs
           </Link>
-          <Link className=" font-normal text-[rgba(61,61,61,1)] text-[16px] " to={"/checkout"}>Checkout</Link>
+          <Link
+            className=" font-normal text-[rgba(61,61,61,1)] text-[16px] "
+            to={"/checkout"}
+          >
+            Checkout
+          </Link>
         </ul>
       </nav>
       <div className=" flex items-center gap-[20px] ">
         <CiSearch className=" w-[24px] h-[24px] text-[rgba(61,61,61,1)] " />
-        <CiShoppingCart className=" w-[24px] h-[24px] text-[rgba(61,61,61,1)] " />
+        <Link to={"/Cart"}>
+          
+          <CiShoppingCart className=" w-[24px] h-[24px] text-[rgba(61,61,61,1)] " />
+        </Link>
         <button className=" w-[100px] h-[35px] bg-[rgba(70,163,88,1)] flex items-center justify-center rounded-[6px] text-white  gap-[5px] outline-0  ">
           <CiLogin /> Login
         </button>
