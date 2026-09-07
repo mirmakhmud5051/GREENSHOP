08da95f8d53cf087ff65c0047bcddc5d094cecc3
Osimxon <osimhondadaxanov2013@gmail.com>
2026-09-07 20:52:12 +0500

    finished my task

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,11 @@
 import CartHero from "../../components/cartHero/CartHero";
+import ShopReleted from "../../components/shopReleted/ShopReleted";
 
 function Cart() {
   return (
     <div>
       <CartHero />
+      <ShopReleted />
     </div>
   );
 }
--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,5 +1,5 @@
-import Footer from "../../components/footer/Footer";
-import Header from "../../components/header/Header";
+import nimadur from "../../assets/nimadur.png";
+import payme from "../../assets/image 17.png";
 
 function Checkout() {
   return (
@@ -134,12 +134,108 @@ function Checkout() {
               <p>Products</p>
               <p>Subtotal</p>
             </div>
-            <hr />
-            <div>
-              <div>
-                <h1>salom</h1>
+            <hr className="text-[#46A35880]" />
+            <div className="w-[405px] h-[70px] bg-[#FBFBFB] mt-[11px] flex items-center mb-[10px]">
+              <img className="w-[70px] h-[70px]" src={nimadur} alt="" />
+              <div className="mr-[40px]">
+                <h3 className="font-medium text-[16px] text-[#3D3D3D]">
+                  Barberton Daisy
+                </h3>
+                <p className="font-normal text-[14px] text-[#727272]">
+                  SKU: 1995751877966
+                </p>
               </div>
+              <p className="font-normal text-[14px] text-[#727272] mr-[40px]">
+                (x 2)
+              </p>
+              <h3 className="font-bold text-[18px] text-[#46A358]">$238.00</h3>
             </div>
+            <div className="w-[405px] h-[70px] bg-[#FBFBFB] mt-[11px] flex items-center mb-[10px]">
+              <img className="w-[70px] h-[70px]" src={nimadur} alt="" />
+              <div className="mr-[40px]">
+                <h3 className="font-medium text-[16px] text-[#3D3D3D]">
+                  Barberton Daisy
+                </h3>
+                <p className="font-normal text-[14px] text-[#727272]">
+                  SKU: 1995751877966
+                </p>
+              </div>
+              <p className="font-normal text-[14px] text-[#727272] mr-[40px]">
+                (x 2)
+              </p>
+              <h3 className="font-bold text-[18px] text-[#46A358]">$238.00</h3>
+            </div>
+            <div className="w-[405px] h-[70px] bg-[#FBFBFB] mt-[11px] flex items-center mb-[17px]">
+              <img className="w-[70px] h-[70px]" src={nimadur} alt="" />
+              <div className="mr-[40px]">
+                <h3 className="font-medium text-[16px] text-[#3D3D3D]">
+                  Barberton Daisy
+                </h3>
+                <p className="font-normal text-[14px] text-[#727272]">
+                  SKU: 1995751877966
+                </p>
+              </div>
+              <p className="font-normal text-[14px] text-[#727272] mr-[40px]">
+                (x 2)
+              </p>
+              <h3 className="font-bold text-[18px] text-[#46A358]">$238.00</h3>
+            </div>
+            <div className="text-end mb-[20px]">
+              <a className="font-normal text-[16px]">
+                Have a coupon code?{" "}
+                <span className="text-[#46A358]">Click here</span>
+              </a>
+            </div>
+            <div className="flex items-center justify-between mb-[10px]">
+              <h3 className="font-normal text-[16px] text-[#3D3D3D]">
+                Subtotal
+              </h3>
+              <b className="font-bold text-[18px] text-[#3D3D3D]">$2,683.00</b>
+            </div>
+            <div className="flex items-center justify-between mb-[10px]">
+              <h3 className="font-normal text-[16px] text-[#3D3D3D]">
+                Coupon Discount
+              </h3>
+              <b className="font-bold text-[18px] text-[#3D3D3D]">(-) 00.00</b>
+            </div>
+            <div className="flex items-center justify-between mb-[10px]">
+              <h3 className="font-normal text-[16px] text-[#3D3D3D]">
+                Shiping
+              </h3>
+              <b className="font-bold text-[18px] text-[#3D3D3D]">$16.00</b>
+            </div>
+            <div className="text-end mb-[17px]">
+              <a className="font-normal text-[14px] text-[#46A358]">
+                View shipping charge
+              </a>
+            </div>
+            <hr className="text-[#46A35880] mb-[16px]" />
+            <div className="flex items-center justify-between mb-[40px]">
+              <h3 className="font-bold text-[18px] text-[#3D3D3D]">Shiping</h3>
+              <b className="font-bold text-[18px] text-[#46A358]">$2,699.00</b>
+            </div>
+            <h2 className="font-bold text-[19px] text-[#3D3D3D] mb-[19px]">
+              Payment Method
+            </h2>
+            <div className="flex items-center gap-[10px] w-full h-[45px] border border-[#EAEAEA] p-[0_11px] rounded-[3px] mb-[15px]">
+              <input type="radio" />
+              <img src={payme} alt="" />
+            </div>
+            <div className="flex items-center gap-[10px] w-full h-[45px] border border-[#EAEAEA] p-[0_11px] rounded-[3px] mb-[15px]">
+              <input type="radio" />
+              <h3 className="font-normal text-[17px] text-[#3D3D3D]">
+                Dorect bank transfer
+              </h3>
+            </div>
+            <div className="flex items-center gap-[10px] w-full h-[45px] border border-[#EAEAEA] p-[0_11px] rounded-[3px] mb-[50px]">
+              <input type="radio" />
+              <h3 className="font-normal text-[17px] text-[#3D3D3D]">
+                Cash on delivery
+              </h3>
+            </div>
+            <button className="p-[12px_160px] bg-[#46A358] rounded-[3px] text-[#FFFFFF] font-bold text-[15px]">
+              Place Order
+            </button>
           </div>
         </div>
       </div>
