5632d823b12f46f61f1bbf4ceb709f037192a041
mirmakhmud5051 <mirmahmudjunaydullayev@gmail.com>
2026-09-07 16:24:02 +0500

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Home from "./pages/home/Home";
 import Cart from "./pages/cart/Cart";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Checkout from './pages/checkout/Checkout';
 
 function App() {
   return (
     <div className=" container flex flex-col  items-center py-[25px]">
       <Header />
      
-      {/* <Home/> */}
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
