2015e8627165f14c6c715dd486b4c1bd6a2874c0
me5zxn <ergashxanovbilolxon@gmail.com>
2026-09-10 23:41:21 +0500

    modal oynala qoshldi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Cart from "./pages/cart/Cart";
@@ -7,24 +7,33 @@ import Footer from "./components/footer/Footer";
 import Checkout from "./pages/checkout/Checkout";
 import Shop from "./pages/shop/Shop";
 import Address from "./pages/address/Address";
-import Modal from "./components/modal/Modal"; 
+import Modal from "./components/modal/Modal";
+import ThankYouModal from "./components/modal/ThankYouModal";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isThankYouOpen, setIsThankYouOpen] = useState(false);
+
   return (
     <div className="container flex flex-col items-center py-[25px]">
-      <Header />
-      
-      
-      <Modal />
+      <Header onLoginClick={() => setIsModalOpen(true)} />
+
+      {isModalOpen && <Modal onClose={() => setIsModalOpen(false)} />}
+      {isThankYouOpen && (
+        <ThankYouModal onClose={() => setIsThankYouOpen(false)} />
+      )}
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
-        <Route path='/address' element={<Address/>}/>
+        <Route path="/address" element={<Address />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
+        <Route
+          path="/checkout"
+          element={<Checkout onPlaceOrder={() => setIsThankYouOpen(true)} />}
+        />
       </Routes>
-      
+
       <Footer />
     </div>
   );
